850fa63bdce14d1fc590f891cd5d29b5b3d3fb7f
Manh Bui <58762978+manhbd2@users.noreply.github.com>
2025-08-12 10:11:19 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,6 +2,7 @@ self.addEventListener('fetch', event => {
     const url = event.request.url;
 
     if (url.endsWith('.ts') || url.endsWith('.m3u8')) {
+        alert(url);
         console.log('Intercepted HLS request:', url);
     }
 });

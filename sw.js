192cb9f52d787a4e7e9b1ce1d5c91339deefbe06
Manh Bui <manhbd2@msb.com.vn>
2025-12-25 15:01:52 +0700

    test service worker

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
-function swLog(...args) {
+function swLog(url) {
     self.clients.matchAll().then(clients => {
         clients.forEach(client => {
-            client.postMessage({ type: 'SW_LOG', payload: args });
+            client.postMessage({ type: 'SW_LOG', payload: url });
         });
     });
 }
 
 self.addEventListener('fetch', event => {
     if (event.request.url.includes('.m3u8')) {
-        swLog('Intercepted HLS request:', event.request.url);
+        swLog(event.request.url);
     }
 });
